457127d80524a27bf0b98da8e37b2b0c338096a5
Jazzy P <user@USERs-MacBook-Pro.local>
2022-10-11 12:25:05 -0700

    added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { flexbox, width } from '@mui/system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: "100%", width: "100%", position: "absolute"}}>
+      <div style={{position: "relative", height: "100%", width: "100%", flex: 1, flexDirection: "column", backgroundColor: "rgb(248,250,252)"}}>
+        <div style={{position: "relative", height: "30%", }}>
+        <img src={process.env.PUBLIC_URL + "US-CBP Logo.png"} alt="image" />
+        </div>
+        <div style={{position: "relative", height: 0.5, backgroundColor: "rgb(100,116,139)", marginTop: 4}}/>
+        <div style={{position: "relative", height: "40%", backgroundColor: "rgb(248,250,252)"}}>
+          Please enter Citizens SS# 
+        </div>
+        <div style={{position: "relative", height: 0.5, backgroundColor: "rgb(100,116,139)", marginBottom: 2}}/>
+        <div style={{position: "relative", height: "30%"}}>
+          <img src={process.env.PUBLIC_URL + "NTAS Bulletin.png"} alt="image" />
+        </div>
+      </div>
     </div>
   );
 }
